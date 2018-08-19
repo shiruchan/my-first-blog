93aa6f67c6b0d9912132d2cab581ddd816bde39f
shiruchan <shiruchan0622@yahoo.co.jp>
2018-08-19 19:01:42 +0900

    Add My Django Girls app

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 以下コメント
     # クライアントがアクセスしてきたURLのドメイン以降に何も指定されていないのであれば、処理をviews.pyのpost_listオブジェクトに渡す。
     # nameで指定した"post_list"を指定しておく事で、テンプレートからこの名前でリンクを張ったりする事ができる。
     path('', views.post_list, name='post_list'),
